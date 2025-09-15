20b61257649e02e4cde0b7d7a09a4568e1503837
sk3775951 <suraj3775951@gmail.com>
2025-09-16 00:38:47 +0530

    Delete C++ Programs/5_Functions_Pointers/01_HelloWorld.cpp

--- C++ Programs/5_Functions_Pointers/01_HelloWorld.cpp
+++
@@ -1,7 +0,0 @@
-// C++ Program to print: Hello World -
-
-#include <iostream>
-using namespace std ;
-int main() {
-    cout <<"Hello World !\n" <<endl <<"I am a C++ Program written by Suraj S." ;
-}
